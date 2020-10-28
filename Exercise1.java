627256b23519d56e4cefcb3392ed51553feaee71
gingerxbread <72840973+gingerxbread@users.noreply.github.com>
2020-10-28 10:45:40 +0800

    Add files via upload

---
+++ Exercise1.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+public class Exercise1{
+	public static void main(String [] nilaiTerima){
+		int noInt = 10,x;
+
+		x = (noInt == 10 ? 3 : 19);
+		System.out.println("x is " + x);
+		// String name;
+		// int num1;
+		// double num2;
+		// String num3;
+		// Scanner input = new Scanner(System.in);
+		// Scanner input2 = new Scanner(System.in);
+		// System.out.println("Enter a String: ");
+		// num3 = input.nextLine(); //or input.nextLine()
+		// System.out.println("String is: " + num3);
+		// System.out.println("Enter a number: ");
+		// num1 = input.nextInt();
+		// System.out.println("Number is: " + num1);
+		// // input.nextLine();
+		// System.out.println("Enter a double: ");
+		// num2 = input.nextDouble();
+		// System.out.println("Double is: " + num2);
+
+		// int num = '5';
+		// System.out.println("Num: " + num);
+		// final static char CHA = 99;
+		// System.out.println("Char: " + CHA);
+		// CHA = 100;
+		// System.out.println("Char: " + CHA);
+		// int [] arrInt = {10,20,30,40};
+		// for (int i=0; i<arrInt.length; i++){
+		// 	System.out.println(arrInt[i]);
+		// }
+		// for (int x : arrInt){
+		// 	System.out.println("for each: " + x);
+		// }
+		// int noInt = 123;
+		// int noInt2 = 456;
+		// double noDou = 123.456;
+		// System.out.format("%6d and %4d",noInt,noInt2);
+		// System.out.format("%7.1f",noDou);
+		// System.out.printf("\n%7.1f %5d",noDou,noInt);
+		// System.out.println("Nilai 1: " + nilaiTerima[0]);
+		// System.out.println("Nilai 2: " + nilaiTerima[1]);
+		// System.out.println("Nilai 3: " + nilaiTerima[2]);
+
+		// int sum = 0;
+
+		// for (int i=0; i<nilaiTerima.length; i++){
+		// 	System.out.println("Nilai: " + nilaiTerima[i]);
+		// 	sum += Integer.parseInt(nilaiTerima[i]);
+		// 	System.out.println("Sum: " + sum);
+		// }
+	}
+}
